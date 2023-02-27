5c0503cb60d38d4b34af6b2552bda1546453f0bf
sazid <dipcse07@gmail.com>
2023-02-27 12:28:01 +0900

    Initial Commit

---
+++ repoWatcher/repoWatcherApp.swift
@@ -0,0 +1,17 @@
+//
+//  repoWatcherApp.swift
+//  repoWatcher
+//
+//  Created by MD SAZID HASAN DIP on 2023/02/27.
+//
+
+import SwiftUI
+
+@main
+struct repoWatcherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
