1b4ae7314b1ec06245bd075a3e408c6ba56f5b1d
sazid <dipcse07@gmail.com>
2023-08-04 12:01:49 +0900

    Initial Commit

---
+++ repoBee/repoBeeApp.swift
@@ -0,0 +1,17 @@
+//
+//  repoBeeApp.swift
+//  repoBee
+//
+//  Created by intel on 2023/08/04.
+//
+
+import SwiftUI
+
+@main
+struct repoBeeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
